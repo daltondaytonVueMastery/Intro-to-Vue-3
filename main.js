8560741998a8d6125da4b12358ecae6d5f60b26b
Dalton Dayton <daltondayton1@gmail.com>
2022-03-13 11:50:28 -0600

    Attribute Binding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description: "Some great socks.",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.google.com",
     };
   },
 });
